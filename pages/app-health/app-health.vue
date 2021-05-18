87d9797252496a677df7a3816cab6a8631af1451
ranmaxli <1206038126@qq.com>
2021-05-18 10:51:47 +0800

    feat:charts add delete last data

--- pages/app-health/app-health.vue
+++ pages/app-health/app-health.vue
@@ -2,8 +2,15 @@
 	<view class="app-health">
 		
 		<view class="charts-box">
-		   <!-- 此处改变的是 seriesTemplate 模板中的默认配置，不必每个series都传smooth:true，将会覆盖:chartData.series 实现更低的代码量 -->
-		   <qiun-data-charts type="line" :opts="{extra:{line:{type:'curve'}}}" :chartData="chartsDataLine1" :echartsH5="true" :echartsApp="true"/>
+
+			<qiun-data-charts type="line"
+			:opts="{extra:{line:{type:'curve'}},
+					enableScroll:true,
+					xAxis:{scrollShow:true,itemCount:4,disableGrid:true}}" 
+			:chartData="chartsDataLine1" 
+			:ontouch="true"
+			:canvas2d="true"/>
+			
 		</view>
 		
 		<view>
@@ -25,14 +32,13 @@
 			<input class="uni-input" maxlength="3" @input="onKeyInput" placeholder="请输入您当前的体重" />
 			<button type="default" @click="insertChatRow">添加</button>
 			<button type="default" @click="deleteChatRow">清空</button>
-			<!-- <button type="default" @click="deleteLastData">删除最近一次数据</button> -->
+			<button type="default" @click="deleteLastData">删除最近一次数据</button>
 			<button type="default" @click="dropTable">删除表</button>
-			<!-- <button type="default" @click="selectSql">查询数据</button> -->
 		</view>
 		
 		<view>
 			<text>
-				{{exeData}}
+				{{allData}}
 			</text>
 		</view>
 		
@@ -62,17 +68,27 @@
 				exeSql:'',
 				exeData:'',
 				maxId:0,
+				allData:'',
 			}
 		},
 		methods: {
 			createChatTable() {
 				let sql = 'CREATE TABLE if not exists ' + this.dbTable +
-					'(`weight` float (32), `createTime` varchar(32));'
-				this.executeSql(sql);
+					' ( "id" int(32) NOT NULL,"weight" float(32),"createTime" varchar(32), PRIMARY KEY ("id"));'
 				
-				// let sql = 'CREATE TABLE if not exists ' + this.dbTable +
-				// 	' ( "id" int(32) NOT NULL,"weight" float(32),"createTime" varchar(32), PRIMARY KEY ("id"));'
-				// this.executeSql(sql)
+				let _this = this;
+				plus.sqlite.selectSql({
+					name: this.dbName,
+					sql: sql,
+					success: function(data) {
+						_this.exeSql = sql;
+						_this.exeData = data;
+						console.log("数据库创建成功！");
+					},
+					fail: function(e) {
+						console.log('selectSql failed: ' + JSON.stringify(e));
+					}
+				});
 			},
 			openDatabase() {
 				plus.sqlite.openDatabase({
@@ -82,7 +98,7 @@
 						console.log('打开数据库成功')
 					},
 					fail: function(e) {
-						console.log(e,'打开数据库失败')
+						// console.log(e,'打开数据库失败')
 					}
 				});
 			},
@@ -109,17 +125,35 @@
 				});
 			},
 			insertChatRow() {
+				let newId =  this.maxId + 1;
+				console.log("当前最大ID为： " + this.maxId);
+				console.log("即将插入的ID为： " + newId);
+				
+				let num = Math.floor(Math.random()*(1 - 100) + 100);
 				let data = this.chatText
-				let sql = "insert into " + this.dbTable +  "(weight, createTime) values(" + data.weight + ",'" + (new Date().getYear() + 1900) + "-" + (new Date().getMonth() + 1) + "-" + new Date().getDate() + "')";
-				this.executeSql(sql)
+				let sql = "insert into " + this.dbTable +  "(id, weight, createTime) values(" + newId + "," + num + ",'" + (new Date().getYear() + 1900) + "-" + (new Date().getMonth() + 1) + "-" + new Date().getDate() + "')";
+				
+				let _this = this;
+				plus.sqlite.selectSql({
+					name: this.dbName,
+					sql: sql,
+					success: function(data) {
+						_this.exeSql = sql;
+						_this.exeData = data;
+						console.log("插入数据成功！");
+					},
+					fail: function(e) {
+						console.log('selectSql failed: ' + JSON.stringify(e));
+					}
+				});
+				
+				this.selectSql();
+				setTimeout(() => {
+					this.getServerData();
+				}, 1000);
 			},
 			insertListChatRow() {
-				// let _this = this;
-				// setTimeout(() => {
-				// 	_this.getMaxId();
-				// 	_this.maxId++
-				// }, 1000);
-				// console.log(this.maxId);
+				// 老方法尚未用到,尚未修改
 				let data = this.chatText1
 				let sql = '';
 				for(let i = 0; i < data.length; i++) {
@@ -129,14 +163,62 @@
 			},
 			deleteChatRow() {
 				let sql = "delete from " + this.dbTable;
-				this.executeSql(sql)
+				
+				let _this = this;
+				plus.sqlite.selectSql({
+					name: this.dbName,
+					sql: sql,
+					success: function(data) {
+						_this.exeSql = sql;
+						_this.exeData = data;
+						console.log("删除表成功！");
+					},
+					fail: function(e) {
+						console.log('selectSql failed: ' + JSON.stringify(e));
+					}
+				});
+				
+				this.selectSql();
+				setTimeout(() => {
+					this.getServerData();
+				}, 1000);
 			},
 			selectSql() {
 				let curPage = 1
 				let pageSize = 100
 				let sql = 'select * from ' + this.dbTable + ' where 1=1' +
 					' limit ' + pageSize + ' offset ' + (curPage - 1) * pageSize;
-				this.executeSql(sql)
+				
+				let _this = this;
+				plus.sqlite.selectSql({
+					name: this.dbName,
+					sql: sql,
+					success: function(data) {
+						_this.exeSql = sql;
+						_this.allData = data;
+						console.log("查询数据成功！");
+					},
+					fail: function(e) {
+						console.log('selectSql failed: ' + JSON.stringify(e));
+					}
+				});
+				
+				setTimeout(() => {
+					this.getServerData();
+				}, 1000);
+				
+				let sql1 = "select max(id) from " + this.dbTable;
+				plus.sqlite.selectSql({
+					name: this.dbName,
+					sql: sql1,
+					success: function(data) {
+						_this.maxId = data[0]["max(id)"];
+						console.log("获取最大id为： " + data[0]["max(id)"]);
+					},
+					fail: function(e) {
+						console.log('selectSql failed: ' + JSON.stringify(e));
+					}
+				});
 			},
 			dropTable() {
 				let sql = 'DROP TABLE ' + this.dbTable + ';'
@@ -163,40 +245,15 @@
 					}
 				});
 			},
-			executeSql(sql) {
-				let _this = this;
-				plus.sqlite.selectSql({
-					name: this.dbName,
-					sql: sql,
-					success: function(data) {
-						_this.exeSql = sql;
-						_this.exeData = data;
-					},
-					fail: function(e) {
-						console.log('selectSql failed: ' + JSON.stringify(e));
-					}
-				});
-				if(sql.indexOf("insert") == 0 || sql.indexOf("delete") == 0 || sql.indexOf("update") == 0){
-					this.selectSql();
-					setTimeout(() => {
-						this.getServerData();
-					}, 1000);
-				}
-				console.log(sql);
-				if(sql.indexOf("max(id)") == 0){
-					_this.maxId = _this.exeData;
-					console.log("当前最大id为： " + getMaxId);
-				}
-			},
 			getServerData() {
 				let _this = this;
 
-				if(_this.exeData.length != 0){
+				if(_this.allData.length != 0){
 					var seriesJsonArray = [];
 					var seriesJsonObject = {};
 					var params1 = [];
-					for(var i = 0; i < _this.exeData.length; i++){
-						params1.push(_this.exeData[i].weight);
+					for(var i = 0; i < _this.allData.length; i++){
+						params1.push(_this.allData[i].weight);
 					}
 					seriesJsonObject['name'] = '体重';
 					seriesJsonObject['data'] = params1;
@@ -204,8 +261,8 @@
 			
 					var resultJsonObject = {};
 					var params2 = [];
-					for(var i = 0; i < _this.exeData.length; i++){
-						params2.push(_this.exeData[i].createTime);
+					for(var i = 0; i < _this.allData.length; i++){
+						params2.push(_this.allData[i].createTime);
 					}
 					resultJsonObject['categories'] = params2;
 					resultJsonObject['series'] = seriesJsonArray;
@@ -220,15 +277,28 @@
 			onKeyInput(event) {
 				this.chatText.weight = event.target.value;
 			},
-			// getMaxId(){
-			// 	let sql = "select max(id) from " + this.dbTable;
-			// 	this.executeSql(sql)
-			// },
-			// deleteLastData(){
-			// 	let sql = "delete from " + this.dbTable + " where 1 order by id desc limit 1";
-			// 	console.log(sql);
-			// 	this.executeSql(sql)
-			// }
+			deleteLastData(){
+				let sql = "delete from " + this.dbTable +" where id = " + this.maxId;
+				
+				let _this = this;
+				plus.sqlite.selectSql({
+					name: this.dbName,
+					sql: sql,
+					success: function(data) {
+						_this.exeSql = sql;
+						_this.exeData = data;
+						console.log("成功删除最新数据！");
+					},
+					fail: function(e) {
+						console.log('selectSql failed: ' + JSON.stringify(e));
+					}
+				});
+				
+				this.selectSql();
+				setTimeout(() => {
+					this.getServerData();
+				}, 1000);
+			}
 		},
 		onReady() {
 			this.openDatabase();
